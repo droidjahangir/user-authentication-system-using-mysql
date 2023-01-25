ce71469f12fdb943aede853963d471d2c6970e39
jahangir <jahangir.droid@gmail.com>
2023-01-25 23:19:50 +0600

    server error handled with throwing custom error

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,29 +1,37 @@
 import generateToken from "../utils/generateToken.js";
 import { getUserByUserId, pool } from "../database.js";
+import { CustomError } from "../utils/error.js";
 
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Private/Admin
 const getUsers = async (req, res) => {
-  const [users] = await pool.query("SELECT * FROM users");
-  for(const user of users) {
-    delete user.password
+  try {
+    const [users] = await pool.query("SELECT * FROM users");
+    for (const user of users) {
+      delete user.password;
+    }
+    res.json(users);
+  } catch (error) {
+    throw new CustomError(400, "users not found");
   }
-  res.json(users);
 };
 
 // @desc    Get user by ID
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserById = async (req, res) => {
-  const id = req.params.id;
-  const user = await getUserByUserId(id);
-  if (user) {
-    delete user.password
-    res.json(user);
-  } else {
-    res.status(404);
-    throw new Error("User not found");
+  try {
+    const id = req.params.id;
+    const user = await getUserByUserId(id);
+    if (user) {
+      delete user.password;
+      res.json(user);
+    } else {
+      throw new CustomError(400, "User not found");
+    }
+  } catch (error) {
+    throw new CustomError(500, 'Server error occured')
   }
 };
 
@@ -31,54 +39,55 @@ const getUserById = async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const [userExists] = await pool.query(
-    `
-        SELECT *
-        FROM users
-        WHERE email = ?
-    `,
-    [email]
-  );
-
-  if (userExists?.length > 0) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
-
-  if (password?.length < 6 || password?.length > 30) {
-    res.status(404);
-    throw new Error("validation failed");
-  }
-
-  if (username && (username.length < 4 || username.length > 20)) {
-    res.status(404);
-    throw new Error("validation failed");
-  }
-
-  const [result] = await pool.query(
-    `
-    INSERT INTO users (email, username, password)
-    VALUES (?, ?, ?)
-    `,
-    [email, username, password]
-  );
-
-  const id = result.insertId;
-  const user = await getUserByUserId(id);
-
-  if (user) {
-    res.status(201).json({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+    const [userExists] = await pool.query(
+      `
+          SELECT *
+          FROM users
+          WHERE email = ?
+      `,
+      [email]
+    );
+  
+    if (userExists?.length > 0) {
+      throw new CustomError(400, 'User already exists')
+    }
+  
+    if (password?.length < 6 || password?.length > 30) {
+      throw new CustomError(400, 'validation failed')
+    }
+  
+    if (username && (username.length < 4 || username.length > 20)) {
+      throw new CustomError(400, 'validation failed')
+    }
+  
+    const [result] = await pool.query(
+      `
+      INSERT INTO users (email, username, password)
+      VALUES (?, ?, ?)
+      `,
+      [email, username, password]
+    );
+  
+    const id = result.insertId;
+    const user = await getUserByUserId(id);
+  
+    if (user) {
+      res.status(201).json({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(id),
+      });
+    } else {
+      throw new CustomError(400, 'Invalid user data')
+    }
+  } catch (error) {
+    console.log('error ===> ', error)
+    throw new CustomError(500, 'Server error occured')
   }
 };
 
@@ -86,45 +95,47 @@ const registerUser = async (req, res) => {
 // @route   PUT /api/users/:id
 // @access  Private
 const updateUser = async (req, res) => {
-  const user = await getUserByUserId(req.user.id);
+  try {
+    const user = await getUserByUserId(req.user.id);
 
-  if (user) {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username && (username.length < 4 || username.length > 20)) {
-      res.status(404);
-      throw new Error("validation failed");
-    }
-    user.name = req.body.name || user.name;
-    user.image = req.body.image || user.image;
-    user.username = req.body.username || user.username;
-    user.email = req.body.email || user.email;
-    if (password) {
-      if (password.length < 6 || password.length > 30) {
-        res.status(404);
-        throw new Error("validation failed");
+    if (user) {
+      const username = req.body.username;
+      const password = req.body.password;
+      if (username && (username.length < 4 || username.length > 20)) {
+        throw new CustomError(400, 'validation failed')
       }
-      user.password = password;
+      user.name = req.body.name || user.name;
+      user.image = req.body.image || user.image;
+      user.username = req.body.username || user.username;
+      user.email = req.body.email || user.email;
+      if (password) {
+        if (password.length < 6 || password.length > 30) {
+          throw new CustomError(400, 'validation failed')
+        }
+        user.password = password;
+      }
+  
+      const updatedUser = await pool.query(
+        `
+          UPDATE users
+          SET username = ?, email = ?, password = ?, name = ?, image = ?
+          WHERE id = ?;
+          `,
+        [user.username, user.email, user.password, user.name, user.image, user.id]
+      );
+  
+      console.log("updated user : ", updatedUser);
+  
+      res.json({
+        ...user,
+        token: generateToken(updatedUser._id),
+      });
+    } else {
+      throw new CustomError(400, 'User not found')
     }
-
-    const updatedUser = await pool.query(
-      `
-        UPDATE users
-        SET username = ?, email = ?, password = ?, name = ?, image = ?
-        WHERE id = ?;
-        `,
-      [user.username, user.email, user.password, user.name, user.image, user.id]
-    );
-
-    console.log("updated user : ", updatedUser);
-
-    res.json({
-      ...user,
-      token: generateToken(updatedUser._id),
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
+  } catch (error) {
+    console.log('error message ===> ', error)
+    throw new CustomError(500, 'Server error occured')
   }
 };
 
@@ -132,20 +143,24 @@ const updateUser = async (req, res) => {
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = async (req, res) => {
-  const user = await getUserByUserId(req.params.id);
+  try {
+    const user = await getUserByUserId(req.params.id);
 
-  if (user) {
-    await pool.query(
-      `
-        DELETE FROM users
-        WHERE id = ?;
-        `,
-      [user.id]
-    );
-    res.json({ message: "User removed" });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
+    if (user) {
+      await pool.query(
+        `
+          DELETE FROM users
+          WHERE id = ?;
+          `,
+        [user.id]
+      );
+      res.json({ message: "User removed" });
+    } else {
+      throw new CustomError(400, 'User not found')
+    }
+  } catch (error) {
+    console.log('error message ===> ', error)
+    throw new CustomError(500, 'Server error occured')
   }
 };
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
-import asyncHandler from "express-async-handler";
 
+import { CustomError } from '../utils/error.js'
 import { getUserByUserId } from "../database.js";
 
 const protect = async (req, res, next) => {
@@ -18,30 +18,32 @@ const protect = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       const userInfo = await getUserByUserId(decoded.id);
-      delete userInfo.password;
+      delete userInfo?.password;
 
       req.user = userInfo
 
       next();
     } catch (error) {
       console.error(error);
-      res.status(401);
-      throw new Error("Not authorized, token failed");
+      throw new CustomError(400, 'Server error occured')
     }
   }
 
   if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
+    throw new CustomError(400, 'Not authorized, no token')
   }
 };
 
 const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401);
-    throw new Error("Not authorized as an admin");
+  try {
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      throw new CustomError(400, 'Not authorized as an admin')
+    }
+  } catch (error) {
+    console.log('error ===> ', error)
+    throw new CustomError(500, 'server error occured')
   }
 };
 
